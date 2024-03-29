4fd95209e2072f7804c2bb522510057fdf14b560
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-03-29 20:24:11 +0700

    Pertemuan 05 - Praktikum 3 - Date and Time

---
+++ pertemuan-5/time.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h3>Time</h3>
+        <?php
+            date_default_timezone_set("asia/jakarta");
+            echo date("h:i:sa");
+        ?>
+    </body>
+</html>
