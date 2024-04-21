5b1a0af02212d2f45d0b15bcb31c0a919e458c31
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-22 05:59:36 +0700

    Pertemuan 08 - Praktikum 1

--- pertemuan-8/upload.php
+++ pertemuan-8/upload.php
@@ -1,22 +1,35 @@
 <?php
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $targetDirectory = "uploads/"; //Direktori tujuan untuk menyimpan file
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
     $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
 
     $maxFileSize = 10 * 1024 * 1024;
 
-    if(in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize){
-        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)){
-            echo "File berhasil diunggah";
-        } else{
+    if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+            echo "File berhasil diunggah<br>";
+            list($width_orig, $height_orig) = getimagesize($targetFile);
+            $width = 200;
+            $height = round(($height_orig / $width_orig) * $width);
+            $image_p = imagecreatetruecolor($width, $height);
+            if ($fileType == 'jpg' || $fileType == 'jpeg') {
+                $image = imagecreatefromjpeg($targetFile);
+            } else if ($fileType == 'png') {
+                $image = imagecreatefrompng($targetFile);
+            } else if ($fileType == 'gif') {
+                $image = imagecreatefromgif($targetFile);
+            }
+            imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+            imagejpeg($image_p, $targetDirectory . 'thumb_' . basename($_FILES["fileToUpload"]["name"]), 100);
+            echo "Thumbnail Berhasil Dibuat";
+        } else {
             echo "Gagal mengunggah file.";
         }
     } else {
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
-
 }
-?>
+    
