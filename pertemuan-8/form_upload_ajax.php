c1aac465d7b9e13d2aa84c18a9ef5813e045d579
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-22 12:43:45 +0700

    Pertemuan 08 - Praktikum 4

--- pertemuan-8/form_upload_ajax.php
+++ pertemuan-8/form_upload_ajax.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <!-- <title>Unggah File Dokumen</title> -->
-        <title>Unggah File Gambar</title>
-    </head>
-    <body>
-        <h3>UPLOAD GAMBAR</h3>
-        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-            <!-- <input type="file" name="file" id="file"> -->
-            <input type="file" name="file" id="file" multiple="multiple" accept=".jpg, .jpeg, .png, .gif">
-            <input type="submit" name="submit" value="Unggah">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="upload.css">
+    <title>Unggah File Dokumen</title> <!-- <title>Unggah File Gambar</title> -->
+</head>
+
+<body>
+    <!-- <h3>UPLOAD GAMBAR</h3>
+    <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file" id="file"> <input type="file" name="file" id="file" multiple="multiple" accept=".jpg, .jpeg, .png, .gif"> 
+        <input type="submit" name="submit" value="Unggah"> 
+    </form>
+    <div id="status"></div> -->
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
+        <form id="upload-form" action="upload.php" method="post" enctype="multipart/form-data">
+            <div class="file-input-container"> 
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label> 
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
         </form>
-        <div id="status"></div>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="upload.js"></script>
-    </body>
+        <div id="status" class="upload-status"></div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
+</body>
+
 </html>
--- pertemuan-8/upload.js
+++ pertemuan-8/upload.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    $('#file').change(function(){
+        if(this.files.length > 0){
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+
     $("#upload-form").submit(function(e){
         e.preventDefault();
 
