72aecf5938126795ceecac454e2c84f4aa0215c4
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-22 22:39:49 +0700

    Pertemuan 10 - Praktikum 4

--- pertemuan-10/proses.php
+++ pertemuan-10/proses.php
@@ -31,6 +31,24 @@
         } else {
             echo "ID tidak valid";
         }
+    } else if ($aksi == "hapus") {
+        if (isset($_GET["id"])) {
+            $id = $_GET["id"];
+
+            $query = "DELETE FROM anggota WHERE id='$id'";
+
+            if(mysqli_query($koneksi, $query)) {
+                header("Location: index.php");
+                exit();
+            } else {
+                echo "Gagal menghapus data : " . mysqli_error($koneksi);
+            }
+        } else {
+            echo "ID tidak valid.";
+        }
+    } else {
+        header("Location: index.php");
+        exit();
     }
 
     mysqli_close($koneksi);
