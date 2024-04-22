cbc4dee466870bf4e3ce584b59b3ac1147cf9010
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-22 23:10:17 +0700

    Pertemuan 10 - Praktikum 6

---
+++ pertemuan-10/ajax/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','Zanuaraldi0101');
+define('DB1', 'prakwebdb');
+
+// Buat Koneksinya
+$db1 = new mysqli(HOST, USER, PASS, DB1);
