a155c5da5ec455fde7a1903ad5cb2aaf303c1838
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-22 22:48:29 +0700

    Pertemuan 10 - Praktikum 5

--- pertemuan-10/edit.php
+++ pertemuan-10/edit.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
-
 <head>
     <title>Edit Data Anggota</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php
+    <php
     include('koneksi.php');
     $id = $_GET['id'];
     $query = "SELECT * FROM anggota WHERE id = $id";
@@ -18,20 +17,73 @@
     <div class="container">
         <h2>Edit Data Anggota</h2>
         <form action="proses.php?aksi=ubah" method="post">
-            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+            <input type="hidden" name="id" value="<php echo $row['id']; ?>">
             <label for="nama">Nama:</label>
-            <input type="text" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
+            <input type="text" name="nama" id="nama" value="<php echo $row['nama']; ?>" required>
             <label for="jenis_kelamin">Jenis Kelamin:</label>
             <div class="radio-group">
-                <input type="radio" name="jenis_kelamin" value="L" id="laki" <?php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required> <label for="laki">Laki-laki</label>
-                <input type="radio" name="jenis_kelamin" value="p" id="perempuan" <?php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required> <label for="perempuan">Perempuan</label>
+                <input type="radio" name="jenis_kelamin" value="L" id="laki" <php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required> <label for="laki">Laki-laki</label>
+                <input type="radio" name="jenis_kelamin" value="p" id="perempuan" <php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required> <label for="perempuan">Perempuan</label>
             </div>
             <label for="alamat">Alamat:</label>
-            <input type="text" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
+            <input type="text" name="alamat" id="alamat" value="<php echo $row['alamat']; ?>" required>
             <label for="no_telp">No. Telp:</label>
-            <input type="text" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required> <button type="submit">Simpan Perubahan</button> <a href="index.php" class="btn-kembali">Kembali</a>
+            <input type="text" name="no_telp" id="no_telp" value="<php echo $row['no_telp']; ?>" required> <button type="submit">Simpan Perubahan</button> <a href="index.php" class="btn-kembali">Kembali</a>
         </form>
     </div>
 </body>
+</html> -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Data Anggota</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<?php
+include('koneksi.php');
+$id = $_GET['id'];
+$query = "SELECT * FROM anggota WHERE id = $id";
+$result = mysqli_query($koneksi, $query);
+$row = mysqli_fetch_assoc($result);
+mysqli_close($koneksi);
+?>
+<div class="container mt-4">
+    <h2>Edit Data Anggota</h2>
 
+    <form action="proses.php?aksi=ubah" method="post">
+        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+        <div class="form-group">
+            <label for="nama">Nama :</label>
+            <input type="text" class="form-control" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="jenis_kelamin">Jenis Kelamin :</label>
+            <div class="form-check">
+                <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" id="laki" <?php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required>
+                <label class="form-check-label" for="laki">Laki-laki</label>
+            </div>
+            <div class="form-check">
+                <input type="radio" class="form-check-input" name="jenis_kelamin" value="P" id="perempuan" <?php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required>
+                <label class="form-check-label" for="perempuan">Perempuan</label>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="alamat">Alamat :</label>
+            <input type="text" class="form-control" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="no_telp">No. Telp :</label>
+            <input type="text" class="form-control" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+    </form>
+
+    <a class="btn btn-secondary mt-2" href="index.php">Kembali</a>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
