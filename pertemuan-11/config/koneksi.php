58e7f2af4fe713b077c2ed0d2ba8cb0be1c3293e
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-22 23:55:54 +0700

    Pertemuan 11 - Praktikum 1

---
+++ pertemuan-11/config/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+$koneksi = mysqli_connect("localhost", "root", "Zanuaraldi0101", "prakwebdb");
+
+if (mysqli_connect_errno()) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
