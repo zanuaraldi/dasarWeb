b52f29f33050e0c0e9613afa209a3b849271bfe4
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-23 00:47:00 +0700

    m

--- pertemuan-11/config/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set("Asia/Jakarta");
-$koneksi = mysqli_connect("localhost", "root", "Zanuaraldi0101", "prakwebdb");
-
-if (mysqli_connect_errno()) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
--- pertemuan-11/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (session_status() === PHP_SESSION_NONE) 
-        session_start();
-    session_destroy();
-
-    header('location: index.php');
-?>
