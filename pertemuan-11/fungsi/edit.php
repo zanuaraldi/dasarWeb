048db1166fcfd4f35176950bb7dcb4276a6ffaed
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-23 10:42:44 +0700

    Pertemuan 11 - Praktikum 5

--- pertemuan-11/fungsi/edit.php
+++ pertemuan-11/fungsi/edit.php
@@ -19,6 +19,56 @@ if (!empty($_SESSION['username'])) {
         }
 
         header("Location: ../index.php?page=jabatan");
-    }    
+    } elseif (!empty($_GET['anggota'])) {
+        $user_id = antiinjection($koneksi, $_POST['id']);
+        $nama = antiinjection($koneksi, $_POST['nama']);
+        $jabatan = antiinjection($koneksi, $_POST['jabatan']);
+        $jenis_kelamin = antiinjection($koneksi, $_POST['jenis_kelamin']);
+        $alamat = antiinjection($koneksi, $_POST['alamat']);
+        $no_telp = antiinjection($koneksi, $_POST['no_telp']);
+        $username = antiinjection($koneksi, $_POST['username']);
+        
+        $query_anggota = "UPDATE anggota SET 
+            nama = '$nama', 
+            jenis_kelamin = '$jenis_kelamin',
+            alamat = '$alamat',
+            no_telp = '$no_telp',
+            jabatan_id = '$jabatan'
+            WHERE user_id = '$user_id'";
+        
+        if (mysqli_query($koneksi, $query_anggota)) {
+            if (!empty($_POST['password'])) {
+                $password = $_POST['password'];
+                // Generate random salt
+                $salt = bin2hex(random_bytes(16));
+                // Gabungkan salt dengan password
+                $combined_password = $salt . $password;
+                // Hash password dengan salt
+                $hashed_password = password_hash($combined_password, PASSWORD_BCRYPT);
+                $query_user = "UPDATE user SET 
+                    username = '$username', 
+                    password = '$hashed_password', 
+                    salt = '$salt' 
+                    WHERE id = '$user_id'";
+                
+                if (mysqli_query($koneksi, $query_user)) {
+                    pesan('success', "Anggota Telah Diubah.");
+                } else {
+                    pesan('warning', "Data Anggota Berhasil Diubah, Tetapi Password Gagal Diubah Karena: " . mysqli_error($koneksi));
+                }
+            } else {
+                $query_user = "UPDATE user SET username = '$username' WHERE id = '$user_id'";
+                if (mysqli_query($koneksi, $query_user)) {
+                    pesan('success', "Anggota Telah Diubah");
+                } else {
+                    pesan('warning', "Data Anggota Berhasil Diubah, Tetapi Username Gagal Diubah Karena: " . mysqli_error($koneksi));
+                }
+            }
+        } else {
+            pesan('danger', "Mengubah Anggota Karena: " . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
+    } else {
+    }   
 }
 ?>
--- pertemuan-11/fungsi/hapus.php
+++ pertemuan-11/fungsi/hapus.php
@@ -16,6 +16,20 @@ if (!empty($_SESSION['username'])) {
         }
 
         header("Location: ../index.php?page=jabatan");
-    } 
+    } elseif (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM anggota WHERE user_id = '$id'";
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM user WHERE id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('danger', "Data Login Tidak Terhapus Karena.: " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+        header("Location: ../index.php?page=anggota");
+    }
 }
 ?>
