ec4d4678b2256788ba2eb90ca0d0974efb55cc06
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-04-25 19:27:20 +0700

    Pertemuan 12 - Praktikum 2

---
+++ pertemuan-12/crud/database.php
@@ -0,0 +1,17 @@
+<?php
+    class Database {
+        private $host = "localhost";
+        private $username = "root";
+        private $password = "Zanuaraldi0101";
+        private $database = "prakwebdb";
+        public $conn;
+
+        public function __construct() {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+        }
+    }
+?>
